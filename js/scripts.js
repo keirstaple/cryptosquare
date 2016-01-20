26e5be7524111c828bf6578baef4f4a08c277f4d
emmanuel cutler and Keir Staple <Keir Staple>
2016-01-19 16:55:50 -0800

    create initial steps for making cryptosquare

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,29 @@
-var triangle = function (sideOne, sideTwo, sideThree){
-  if (isTriangle(sideOne, sideTwo, sideThree)){
-    return triangleType(sideOne, sideTwo, sideThree);
-  } else {
-    return "not a triangle.";
-  }
-};
-
-var isTriangle = function(sideOne, sideTwo, sideThree) {
-  return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo);
-};
-
-var triangleType = function(sideOne, sideTwo, sideThree){
-  if (sideOne===sideTwo && sideTwo===sideThree) {
-    return "equilateral";
-  } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
-    return "isosceles";
-  } else {
-    return "scalene";
-  }
-}
-
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
-    debugger;
-    var sideOne = parseInt($("input#sideOne").val());
-    var sideTwo = parseInt($("input#sideTwo").val());
-    var sideThree = parseInt($("input#sideThree").val());
 
-    var result= triangle(sideOne, sideTwo, sideThree);
+ // debugger;
+    var userInput = $('input#enter').val();
 
-    $("#result").append(result);
+    var alphaNumeric = userInput.replace(/[^A-ZA-z]/g,"").toLowerCase();
+
+    var countChars = alphaNumeric.length;
+
+    var squareRoot = Math.sqrt(countChars);
+
+    var roundInteger = Math.round(squareRoot);
+
+
+    // function addNewlines(str) {
+    //   var finalmessage = '';
+    //   while (str.length > 0) {
+    //     finalmessage += str.substring(0, roundInteger) + '\n';
+    //     str = str.substring(roundInteger);
+    //   }
+    //   return finalmessage;
+    // }
+
+    $("#result").text(alphaNumeric);
+
   event.preventDefault();
   });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,36 +1,35 @@
-describe ('isTriangle', function(){
-  it("determines if 3 sides create a valid triangle", function(){
-    expect(isTriangle (2,3, 10)).to.equal(false)
-  });
-
-  it("determines if 3 sides create a valid triangle", function(){
-    expect(isTriangle(2,2,2)).to.equal(true)
+describe ('alphaNumeric', function(){
+  it("removes spaces and punctuations", function(){
+    expect(alphaNumeric("Life is Cool.")).to.equal("LifeisCool")
   });
 });
 
-describe('triangleType', function(){
-  it("determine if the 3 sides make an equilateral triangle", function(){
-    expect(triangleType (2,2,2)).to.equal("equilateral")
+describe ('downCase', function(){
+  it("converts string to lowercase", function(){
+    expect(downCase("LifeisCool")).to.equal("lifeiscool")
   });
 });
 
-describe('triangleType', function(){
-  it("determine if the 3 sides make an isosceles triangle", function(){
-    expect(triangleType (2,2,3)).to.equal("isosceles")
+describe ('countChars', function(){
+  it("counts the character number in the string", function(){
+    expect(countChars("lifeiscool")).to.equal("10")
   });
 });
 
-describe('triangleType', function(){
-  it("determine if the 3 sides make an scalene triangle", function(){
-    expect(triangleType (2,3,4)).to.equal("scalene")
+describe ('squareRoot', function(){
+  it("takes the square root of the number of characters", function(){
+    expect(squareRoot("10")).to.equal("3.16")
   });
 });
 
-describe('triangle', function(){
-  it("determines if 3 sides make a scalene triangle", function(){
-    expect(triangle(2,3,4)).to.equal("scalene")
+describe ('roundInteger', function(){
+  it("takes the square root of the number of characters and rounds to the nearest whole integer", function(){
+    expect(roundInteger("3.16")).to.equal("3")
   });
-  it("determines if 3 sides create a valid triangle", function(){
-    expect(triangle(2,2,8000)).to.equal("These sides do not a triangle make.")
+});
+
+describe ('lineLength', function(){
+  it("makes all line lengths= roundInteger", function(){
+    expect(lineLength("3.16")).to.equal("3")
   });
 });
